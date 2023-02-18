fd102d493eacb0ae625ca95fa1963abe3f7fb06e
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-18 17:41:19 +0100

    fixing missing variables definition

--- variables.tf
+++ variables.tf
@@ -1,3 +1,28 @@
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region to use."
+  type        = string
+}
+
+variable "location_short" {
+  description = "Short string for Azure location."
+  type        = string
+}
+
+variable "environment" {
+  description = "Project environment."
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name."
+  type        = string
+}
+
 variable "create_mode" {
     description = "The creation mode for the CosmosDB Account. Possible values are Default and Restore. Changing this forces a new resource to be created."
     type = string
